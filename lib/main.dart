da9989e7a67fe986f53f19fa16751ded7b771bde
Kunal <shivamgeu7@gmail.com>
2023-04-11 19:25:33 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:catalog_app/screens/home_page.dart';
 import 'package:catalog_app/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily
       ),
-      initialRoute: '/home',
+      initialRoute: '/',
       routes:  {
         '/':(context) => const LoginPage(),
         '/home':(context) => const HomePage(),
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -5,9 +5,39 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(child: Text('Login')),
-      
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/login.png',
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Catalogue Login',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(hintText: "Enter Username"),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: "Enter Password"),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
